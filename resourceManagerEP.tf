c70e7ca2da21b81072ef1fa43cdef9a9d5b80935
Martín Gutiérrez <martin.gutierrez@oracle.com>
2022-06-10 10:09:25 -0400

    Normal

---
+++ resourceManagerEP.tf
@@ -0,0 +1,6 @@
+resource "oci_resourcemanager_private_endpoint" "rms_private_endpoint" {
+  compartment_id = var.compartment_ocid
+  display_name   = "${var.apodo]-rms-endpoint"
+  vcn_id         = var.vcn
+  subnet_id      = var.dmz
+}
