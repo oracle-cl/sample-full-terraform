8b259d31cd4182748fa923b294fbb562e9bfa065
Martín Gutiérrez <martin.gutierrez@oracle.com>
2022-06-10 11:59:58 -0400

    Normal

--- compute.tf
+++ compute.tf
@@ -11,10 +11,10 @@ resource oci_core_instance c {
   }
   display_name = "${var.apodo}_app_${count.index}"
   metadata = { "ssh_authorized_keys" = local.ssh_public_key }
-  shape = "VM.Standard.E4.Flex"
+  shape = var.shape
   shape_config {
-    ocpus = 1
-    memory_in_gbs = 8
+    ocpus = var.ocpu
+    memory_in_gbs = var.memoria
   }
   source_details {
     source_type = "image"
--- parametros.tf
+++ parametros.tf
@@ -1,13 +1,18 @@
 variable "apodo" {}
 variable "adomain" {}
 
+variable "comparedes" {}
 variable "vcn" {}
 variable "reddmz" {}
 variable "reddata" {}
 variable "redapp" {}
 variable "nsg" {}
+
 variable "password" {}
 variable "cantidad" {}
+variable "shape" {}
+variable "ocpu" {}
+variable "memoria" {}
 
 variable "compartment_ocid" {}
 variable "ssh_public_key" {}
